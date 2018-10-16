ac5c5af3e753624e0281250ea8e8aa7ae57838a9
Prasad Siri <siri.prasad@gmail.com>
2018-10-16 13:27:22 +0530

    Prime number problem added

---
+++ Prime.py
@@ -0,0 +1,15 @@
+n = 10
+x = 3
+l = [2]
+count = 0
+while n > count:
+    flag = 1
+    for y in range(2, x):
+        if x % y == 0:
+            flag = 0
+            break
+    if flag == 1:
+        l.append(x)
+        count += 1
+    x = x + 1
+print(l)
